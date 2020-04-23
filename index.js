ecbb794a7d6291013762ed44aa27d5c25bf5239b
Jeff <JeffreyRiggle@gmail.com>
2020-04-23 06:57:19 -0400

    another change to make sona integration easier

--- index.js
+++ index.js
@@ -2,25 +2,23 @@ const { SES } = require('aws-sdk');
 const ses = new SES({region: 'us-east-1'});
 
 const sendMail = (event, context, callback) => {
-    const body = event.body;
-
     const options = {
         Destination: {
-            ToAddresses: [body.toAddress]
+            ToAddresses: [event.toAddress]
         },
         Message: {
             Body: {
                 Html: {
                     Charset: "UTF-8",
-                    Data: body.html
+                    Data: event.html
                 }
             },
             Subject: {
                 Charset: "UTF-8", 
-                Data: body.subject
+                Data: event.subject
             }
         },
-        Source: body.from
+        Source: event.from
     };
     
     ses.sendEmail(options, (err, data) => {
